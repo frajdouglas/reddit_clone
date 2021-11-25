179b508878ca215a73eee16c22a62f65394d3381
frajdouglas <87184070+frajdouglas@users.noreply.github.com>
2021-11-25 15:58:38 +0000

    Update topics.controller.js

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -4,7 +4,7 @@ const {selectTopics} = require('../models/topics.model')
 
 
 exports.getTopics = (req,res,next) => {
-    console.log("IN THE COMMENTS CONTROLLERS FILE IN THE FUNCTION getTopics")
+    console.log("IN THE TOPICS CONTROLLERS FILE IN THE FUNCTION getTopics")
     selectTopics()
     .then((databaseResponse) => {
         res.status(200).send(databaseResponse);
